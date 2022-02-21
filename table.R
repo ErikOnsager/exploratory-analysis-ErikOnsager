9efd4b276108194868fb31430f6d1b7ca21c602b
Erik Onsager <eonsag@uw.edu>
2022-02-21 11:35:43 -0800

    Created table organized by top score per city

--- table.R
+++ table.R
@@ -13,8 +13,6 @@ data <- select(data, c(School.Name, City, Student.Enrollment, Percent.Tested,
         "Student Enrollment" = Student.Enrollment, "Percent Tested" = Percent.Tested, "Percent White" = Percent.White, 
         "Percent Black" = Percent.Black, "Percent Asian" = Percent.Asian, "Percent Hispanic" = Percent.Hispanic)
 
-top.twenty <- data %>% arrange(desc(`Average SAT`)) %>% slice(1:20)
 top.each.city <- data %>% group_by(City) %>% slice_max(order_by = `Average SAT`, n = 1)
 
-kable(top.twenty) %>% kable_styling(bootstrap_options = "striped", full_width = F)
 kable(top.each.city) %>% kable_styling(bootstrap_options = "striped", full_width = F)
