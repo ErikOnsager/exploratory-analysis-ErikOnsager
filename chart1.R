c33d52a680ac9d557a78f5aa79f0140d048d26b2
henrystirrat <hstirrat@uw.edu>
2022-02-20 21:07:37 -0800

    created chart of percent tested vs average sat score and completed summary value calculations

--- chart1.R
+++ chart1.R
@@ -1,7 +1,15 @@
-x_values <- seq(1, 3)
-y_values <- seq(1,3)
+library("dplyr")
+
+sat_data <- read.csv("https://raw.githubusercontent.com/info-201b-wi22/exploratory-analysis-ErikOnsager/main/scores.csv?token=GHSAT0AAAAAABQLRLGJJP7QGPHDDPEVR4SMYQ4IHPQ")
+
+sat_data <- sat_data %>% mutate(average_total_score = Average.Score..SAT.Math. + Average.Score..SAT.Reading. + Average.Score..SAT.Writing.)
+
+sat_data$Percent.Tested <- gsub("%$", "", sat_data$Percent.Tested)
+sat_data$Percent.Tested <- as.numeric(sat_data$Percent.Tested)
+Percent_Tested <- sat_data$Percent.Tested
+Average_Score <- sat_data$average_total_score
 
 library(ggplot2)
 ggplot() +
-  geom_point(aes(x=x_values, y = y_values))
+  geom_point(aes(x = Percent_Tested, y = Average_Score))
 
---
+++ summary.R
@@ -0,0 +1,31 @@
+library("dplyr")
+
+sat_data <- read.csv("https://raw.githubusercontent.com/info-201b-wi22/exploratory-analysis-ErikOnsager/main/scores.csv?token=GHSAT0AAAAAABQLRLGJJP7QGPHDDPEVR4SMYQ4IHPQ")
+
+summary_info <- list()
+
+sat_data <- sat_data %>% mutate(average_total_score = Average.Score..SAT.Math. + Average.Score..SAT.Reading. + Average.Score..SAT.Writing.)
+
+summary_info$average_score <- mean(sat_data$average_total_score, na.rm = TRUE)
+
+summary_info$median_score <- median(sat_data$average_total_score, na.rm = TRUE)
+
+# removing the % from the column for percent white and black and changing values to numeric
+sat_data$Percent.White <- gsub("%$", "", sat_data$Percent.White)
+sat_data$Percent.Black <- gsub("%$", "", sat_data$Percent.Black)
+sat_data$Percent.White <- as.numeric(sat_data$Percent.White)
+sat_data$Percent.Black <- as.numeric(sat_data$Percent.Black)
+
+summary_info$percent_white_of_max_tested <- sat_data %>% filter(Percent.Tested == max(Percent.Tested, na.rm = TRUE)) %>% pull(Percent.White)
+summary_info$percent_black_of_max_tested <- sat_data %>% filter(Percent.Tested == max(Percent.Tested, na.rm = TRUE)) %>% pull(Percent.Black)
+
+# Multiple schools had 0% tested, so the average of these schools is reported
+summary_info$percent_white_of_min_tested <- mean(sat_data %>% filter(Percent.Tested == "0.0%") %>% pull(Percent.White))
+summary_info$percent_black_of_min_tested <- mean(sat_data %>% filter(Percent.Tested == "0.0%") %>% pull(Percent.Black))
+
+summary_info$average_score_of_max_enrollment <- sat_data %>% filter(Student.Enrollment == max(Student.Enrollment, na.rm = TRUE)) %>% pull(average_total_score)
+
+#NA because noone at the smallest school took the sat
+summary_info$average_score_of_min_enrollment <- sat_data %>% filter(Student.Enrollment == min(Student.Enrollment, na.rm = TRUE)) %>% pull(average_total_score)
+
+
